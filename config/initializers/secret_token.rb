f846c9585eeb1fcf1734a5e0fe1e5e4b0c27a21a
david <simpleaznx@gmail.com>
2013-08-15 12:47:27 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Parkwood::Application.config.secret_token = '3dce616447d4e5c160ec9eee544aff5ed072ab8147ccb337a9c33777d0ffd9f091db0cadd1bd43f6c06fede188fcd8126ddb4c0c95a11623034b78786f72edca'
